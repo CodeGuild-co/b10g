e48abdea9f82d1848125fc9b9f1866a7db476128
SyedArafatQureshi <syedarafatqureshi@gmail.com>
2016-02-12 11:49:44 +0000

    Update index.php

--- web/index.php
+++ web/index.php
@@ -53,12 +53,6 @@ $count = fread($fp, 1024);
 // Close the file
 fclose($fp);
 
-// Add 1 to the existing count
-$count = $count + 1;
-
-// Display the number of hits
-// If you don't want to display it, comment out this line
-echo "<p>Page views:" . $count . "</p>";
 
 // Reopen the file and erase the contents
 $fp = fopen("counterlog.txt", "w");
