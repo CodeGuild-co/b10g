e0736335d4777cbf99abb7174466d69095c15e05
SyedArafatQureshi <syedarafatqureshi@gmail.com>
2016-02-12 11:51:02 +0000

    Update index.php

--- web/index.php
+++ web/index.php
@@ -32,37 +32,4 @@ $app->get('/subscribe/{name}/', function($name) use($app) {
     return $app['twig']->render('subscribe/' . $name . '.twig');
 });
 
-
-
-
-/**
- * Create an empty text file called counterlog.txt and 
- * upload to the same directory as the page you want to 
- * count hits for.
- * 
- * Add this line of code on your page:
- * <?php include "text_file_hit_counter.php"; ?>
- */
-
-// Open the file for reading
-$fp = fopen("counterlog.txt", "r");
-
-// Get the existing count
-$count = fread($fp, 1024);
-
-// Close the file
-fclose($fp);
-
-
-// Reopen the file and erase the contents
-$fp = fopen("counterlog.txt", "w");
-
-// Write the new count to the file
-fwrite($fp, $count);
-
-// Close the file
-fclose($fp);
-
-
-
 $app->run();
